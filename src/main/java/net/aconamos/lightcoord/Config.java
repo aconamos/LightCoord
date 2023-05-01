274ec343af0078e2e7a845528d2f6ef358cbe5a7
nephi <nephi.norton@gmail.com>
2023-04-30 22:25:52 -0600

    License + margin change (i like it better)
    Signed-off-by: nephi <nephi.norton@gmail.com>

--- src/main/java/net/aconamos/lightcoord/Config.java
+++ src/main/java/net/aconamos/lightcoord/Config.java
@@ -1,8 +1,8 @@
 package net.aconamos.lightcoord;
 
 public class Config {
-    public static float x_offset = 5;
-    public static float y_offset = 5;
+    public static float x_offset = 2;
+    public static float y_offset = 2;
     public static float y_margin = 10;
     public static int color = 0xFFFFFF;
 }
